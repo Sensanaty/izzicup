2a3124c5e3b9d78ef9624b5863e884af94717412
Luka Salevic <salevic@lu-ka.me>
2020-05-04 20:33:46 +0200

    Add 20 tests for parts & make them pass

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,2 +1,16 @@
 class Part < ApplicationRecord
+  validates :part_number, :date_added, :description, :quantity_available, :condition, :base_price, :cost, :minimum_order, :quote_type, :tag, presence: true
+  validates_numericality_of :quantity_available, :base_price, :cost, :minimum_order, greater_than: 0
+  validates_numericality_of :minimum_order, less_than_or_equal_to: :quantity_available, unless: -> { quantity_available.nil? || quantity_available == 0 }
+  before_save :upcase_fields
+
+  private
+
+  def upcase_fields
+    self.part_number = self.part_number.upcase
+    self.description = self.description.upcase
+    self.condition = self.condition.upcase
+    self.quote_type = self.quote_type.upcase
+    self.tag = self.tag.upcase
+  end
 end
