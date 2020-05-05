7c7595ccf2b29004df4dea7ad0896d75929c3fb1
Luka Salevic <salevic@lu-ka.me>
2020-05-05 19:14:54 +0200

    Add all Parts Controller methods

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,10 +1,34 @@
 class PartsController < ApplicationController
+  before_action :set_part, only: [:edit, :update, :show]
   def index
-    @parts = Part.all
     @paginated = Part.paginate(page: params[:page], per_page: 10)
   end
 
   def show
+  end
+
+  def new
+    @part = Part.new
+  end
+
+  def edit
+  end
+
+  def update
     @part = Part.find(params[:id])
+    if @part.update(params[part_params])
+      redirect_to part_path(@part)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def set_part
+    @part = Part.find(params[:id])
+  end
+
+  def part_params
+    params.require(:part).permit!
   end
 end
