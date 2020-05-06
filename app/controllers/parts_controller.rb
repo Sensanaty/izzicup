12d98cc244872f426394f8e8d504211c9ed70cf0
Luka Salevic <salevic@lu-ka.me>
2020-05-06 20:20:22 +0200

    Remove will_paginate gem and references in favor of Pagy

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,7 +1,6 @@
 class PartsController < ApplicationController
   before_action :set_part, only: [:edit, :update, :show]
   def index
-    @paginated = Part.paginate(page: params[:page], per_page: 10)
   end
 
   def show
